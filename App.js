bd0e33ff32a687f17c1ca1e768556e77fa8a7c2f
Pedro Fernandes <contato.phfernandes@gmail.com>
2022-04-09 01:40:34 -0300

    - Conexão com a API HG Weather

--- App.js
+++ App.js
@@ -6,7 +6,9 @@ export default function App() {
 
   const [localizacao, setLocalizacao] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [dados, setDados] = useState(Object)
 
+  /** Obtendo Geolocalizacao com Location */ 
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -21,17 +23,37 @@ export default function App() {
 
   if (errorMsg) {
   } else if (localizacao) {
-   
     var lat = localizacao.coords.latitude;
     var long = localizacao.coords.longitude;
-
     console.log("Latituide: ", lat)
     console.log("Longitude: ", long)
-
   }
+
+  /**Fetch API */
+  let count = 1;
+  useEffect(() => {
+    obtemClima(lat, long)
+  }, [count]); // Good!
+
+
+  async function obtemClima(lat, long) {
+    const url = `https://api.hgbrasil.com/weather?format=json-cors&key=process.env.KEY&lat=-${lat}&lon=${long}&user_ip=remote`
+    await fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const dados = {
+          temperatura: data.results.temp,
+        }
+        setDados(dados);
+      })
+      .catch(function (error) {
+        console.error('Houve um problema ao efetuar a requisição: ' + error.message);
+      });
+  }
+
   return (
     <View>
-      <Text>teste</Text>
+      <Text>Temperatura: {dados.temperatura}</Text>
     </View>
   );
 }
